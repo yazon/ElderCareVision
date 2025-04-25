4bdbc4e5440c1ec2c3c17ad9a3a34d5346abd492
Damian Baranski <damian.baranski@s3connectedhealth.com>
2025-04-25 20:45:49 +0200

    Added emergency call tools

---
+++ src/elder_care_vision/emengencyCall/img_analizer.py
@@ -0,0 +1,7 @@
+class ImgAnalizer:
+    def __init__(self):
+        pass
+
+    def analize_image(self, image_path: str) -> bool:
+        """Analize the image and return True if it is an emergency call, False otherwise."""
+        return True
